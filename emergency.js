95ee9e4f566a4d9088a373e70e374c72d081fe28
maishaj <maishajannat388@gmail.com>
2025-08-29 17:57:54 +0600

    call-button's functionality completed

--- emergency.js
+++ emergency.js
@@ -1,4 +1,4 @@
-
+let call_list=[];
 //Heart Icons
 let hearts=document.getElementsByClassName("heart");
 for(const heart of hearts)
@@ -9,5 +9,58 @@ for(const heart of hearts)
         counts++;
         z.innerText=counts;
         console.log(heart);
+    })
+}
+
+//Call Buttons
+
+let calls=document.getElementsByClassName("call-btns");
+
+for(const call of calls)
+{
+    call.addEventListener('click',function(){
+
+        let coins=document.getElementById("coin-count");
+        let countOfCoins=coins.innerText;
+        
+        if(countOfCoins<20)
+        {
+            alert("You don't have enough coins to make a call!");
+            return;
+        }
+        
+        countOfCoins-=20;
+        coins.innerText=countOfCoins;
+
+        let m=call.parentNode.parentNode;
+        let service=m.getElementsByClassName("service-name");
+        let number=m.getElementsByClassName("service-num");
+        alert("Calling...\n"+service[0].innerText+"\n"+number[0].innerText);
+        let data={
+            sName:service[0].innerText,
+            sNum:number[0].innerText,
+            sDate:new Date().toLocaleTimeString()
+        }
+
+        console.log(data);
+        call_list.push(data);
+
+
+        callHisParent=document.getElementById("history");
+        callHisParent.innerText="";
+        for(const a of call_list)
+        {
+            const div=document.createElement("div");
+            div.innerHTML=`
+            <div class="flex justify-between items-center bg-[#fafafa] p-3 rounded-sm">
+                <div>
+                <h2 class="text-[18px] font-semibold">${a.sName}</h2>
+                <p class="text-[18px] font-medium text-[#5c5c5c]">${a.sNum}</p>
+                </div>
+                <p class="text-[18px] font-400 text-[#111111]">${a.sDate}</p>
+            </div>
+            `
+            callHisParent.append(div);
+        }
     })
 }
